4156cb00213b05f2a659a21da546de7acd8f0be8
Jonathan Berrisch <jonathan@berrisch.biz>
2021-11-25 16:16:49 +0100

    Update RcppClock.h for improved performance and simplicity
    Use a map to match timers in tick() and tock()
    Postpone calculation of durations to the stop() function

--- inst/include/RcppClock.h
+++ inst/include/RcppClock.h
@@ -1,38 +1,69 @@
 #include <Rcpp.h>
 #include <chrono>
+#include <string>
+#include <map>
 
-namespace Rcpp {
-  class Clock {
-    private:
-    std::vector<std::chrono::steady_clock::time_point> tick_times;
-    std::vector<std::string> tick_names, durations_names;
-    std::vector<double> durations;
+#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
+#define now() std::chrono::high_resolution_clock::now()
 
-    public:
+typedef std::map<std::pair<std::string, int>, std::chrono::high_resolution_clock::time_point> TimesMap;
 
+namespace Rcpp
+{
+  class Clock
+  {
+  private:
+    std::vector<double> timers;
+    size_t ticks, tocks = 0;
+    TimesMap tickmap, tockmap;
+    std::pair<std::string, int> key;
+
+  public:
     // start a timer
-    void tick(std::string name) {
-      tick_names.push_back(name);
-      tick_times.resize(tick_times.size() + 1);
-      tick_times.back() = std::chrono::steady_clock::now();
+    void tick(std::string name)
+    {
+      key.first = name;
+      key.second = ticks;
+      tickmap.insert(
+          std::pair<std::pair<std::string, int>, std::chrono::high_resolution_clock::time_point>(key, now()));
+      ticks += 1;
     }
 
     // stop a timer
-    void tock(std::string name) {
-      std::chrono::steady_clock::time_point tock_time = std::chrono::steady_clock::now();
-      durations_names.push_back(name);
-      for (size_t i = tick_names.size(); i > 0; --i) {
-        if (tick_names[i - 1] == name) {
-          durations.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(tock_time - tick_times[i - 1]).count());
-          return;
-        }
-      }
-      Rcpp::stop("Rcpp::Clock 'tock' with name of '" + name + "' did not match with a corresponding 'tick' name");
+    void tock(std::string name)
+    {
+      key.first = name;
+      key.second = tocks;
+      tockmap.insert(
+          std::pair<std::pair<std::string, int>, std::chrono::high_resolution_clock::time_point>(key, now()));
+      tocks += 1;
     }
 
-    // return timer durations
-    void stop(std::string var_name) {
-      DataFrame df = DataFrame::create(Named("ticker") = durations_names, Named("timer") = durations);
+    // calculate timer durations
+    void stop(std::string var_name)
+    {
+      std::vector<std::string> keys;
+      keys.reserve(tickmap.size());
+      std::vector<std::chrono::high_resolution_clock::time_point> ticks;
+      ticks.reserve(tickmap.size());
+      for (auto kv : tickmap)
+      {
+        keys.push_back(kv.first.first);
+        ticks.push_back(kv.second);
+      }
+
+      std::vector<std::chrono::high_resolution_clock::time_point> tocks;
+      tocks.reserve(tockmap.size());
+      for (auto kv : tockmap)
+      {
+        tocks.push_back(kv.second);
+      }
+
+      for (std::size_t i = 0; i < ticks.size(); ++i)
+      {
+        timers.push_back(duration(tocks[i] - ticks[i]));
+      }
+      DataFrame df = DataFrame::create(Named("ticker") = keys, Named("timer") = timers);
       df.attr("class") = "RcppClock";
       Environment env = Environment::global_env();
       env[var_name] = df;
