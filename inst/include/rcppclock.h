2a83bb407a12c178228f58b0384113305f01008f
Jonathan Berrisch <Jonathan@Berrisch.biz>
2024-02-22 19:33:51 +0100

    Add persistent data storage to rcppclock
    that means aggregate does update (not recalculate everything) if it was called earlier.

--- inst/include/rcppclock.h
+++ inst/include/rcppclock.h
@@ -18,32 +18,28 @@ inline int omp_get_thread_num() { return 0; }
 namespace Rcpp
 {
   template <typename T>
-  void remove_duplicates(std::vector<T> &vec)
+  std::vector<T> remove_duplicates(std::vector<T> vec)
   {
     std::sort(vec.begin(), vec.end());
     vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+    return (vec);
   }
 
   namespace sc = std::chrono;
 
   class Clock
   {
-    using tp = sc::high_resolution_clock::time_point;
+    using hr_clock = sc::high_resolution_clock;
     using keypair = std::pair<std::string, unsigned int>;
-    using timesmap = std::map<keypair, tp>;
 
   private:
-    std::string name;                      // Name of R object to return
-    timesmap tickmap;                      // Map of start times
-    std::vector<std::string> tags,         // Vector of identifiers
-        unique_tags;                       // Vector of unique identifiers
-    std::vector<unsigned long int> counts; // Count occurence of identifiers
-    std::vector<double> means, vars;       // Output vecs of mean and sd
-    std::vector<unsigned long long int>    // Observed durations
-        timers;
+    std::string name; // Name of R object to return
+    // Data to be returned: Tag, Mean, SD, Count
+    std::map<std::string, std::tuple<double, double, unsigned long int>> data;
 
-    // Summary object: Tag,
-    std::map<std::string, std::tuple<double, double, unsigned long int>> timers_new;
+    std::map<keypair, hr_clock::time_point> ticks; // Map of start times
+    std::vector<std::string> tags;                 // Vector of identifiers
+    std::vector<unsigned long long int> durations; // Vector of durations
 
   public:
     // Init - Set name of R object
@@ -56,7 +52,7 @@ namespace Rcpp
       keypair key(std::move(tag), omp_get_thread_num());
 
 #pragma omp critical
-      tickmap[key] = sc::high_resolution_clock::now();
+      ticks[key] = hr_clock::now();
     }
 
     // stop a timer - calculate time difference and save key
@@ -67,10 +63,9 @@ namespace Rcpp
 
 #pragma omp critical
       {
-        timers.push_back(
+        durations.push_back(
             sc::duration_cast<sc::microseconds>(
-                sc::high_resolution_clock::now() -
-                tickmap[key])
+                hr_clock::now() - ticks[key])
                 .count());
         tags.push_back(std::move(key.first));
       }
@@ -79,38 +74,52 @@ namespace Rcpp
     // Pass data to R / Python
     void aggregate()
     {
-      // Create copy of tags called unique_names
-      unique_tags = tags;
-      remove_duplicates(unique_tags);
+
+      // Vector of unique identifiers
+      std::vector<std::string> unique_tags = remove_duplicates(tags);
 
       for (unsigned int i = 0; i < unique_tags.size(); i++)
       {
-        unsigned long int count = 0;
-        double mean = 0, M2 = 0;
 
+        std::string tag = unique_tags[i];
+
+        unsigned long int count;
+        double mean, M2;
+
+        // Init
+        if (data.count(tag) == 0)
+        {
+          count = 0;
+          mean = 0;
+          M2 = 0;
+        }
+        else
+        {
+          mean = std::get<0>(data[tag]);
+          M2 = std::get<1>(data[tag]);
+          count = std::get<2>(data[tag]);
+        }
+
+        // Update
         for (unsigned long int j = 0; j < tags.size(); j++)
         {
-          if (tags[j] == unique_tags[i])
+          if (tags[j] == tag)
           {
             // Welford's online algorithm for mean and variance
             count++;
-            double delta = timers[j] - mean;
+            double delta = durations[j] - mean;
             mean += delta / count;
-            M2 += delta * (timers[j] - mean);
+            M2 += delta * (durations[j] - mean);
           }
         }
 
-        // Save count
-        counts.push_back(count);
-
-        // Save average, round to 3 decimal places
-        means.push_back(mean);
-
-        // Calculate sample variance
-        double variance = M2 / (count);
-        // Save standard deviation, round to 3 decimal places
-        vars.push_back(variance);
+        // Save
+        data[tag] = std::make_tuple(mean, M2, count);
       }
+
+      tags.clear();
+      durations.clear();
+      ticks.clear();
     }
 
     // Pass data to R / Python
@@ -118,24 +127,39 @@ namespace Rcpp
     {
       aggregate();
 
+      // Output Objects
+      std::vector<std::string> out_tags;
       std::vector<unsigned long int> out_counts;
-      std::vector<double> out_means, out_sds; // Output vecs of mean and sd
+      std::vector<double> out_means, out_sd;
 
-      for (unsigned int i = 0; i < counts.size(); i++)
+      for (auto const &ent1 : data)
       {
-        out_counts.push_back(counts[i]);
-        out_means.push_back(std::round(means[i]) * 1e-3);
-        out_sds.push_back(std::round(std::sqrt(vars[i] * 1e-6) * 1e+3) * 1e-3);
+        // Save tag
+        out_tags.push_back(ent1.first);
+
+        unsigned long int count = std::get<2>(ent1.second);
+        double mean = std::get<0>(ent1.second);
+        double variance = std::get<1>(ent1.second) / count;
+
+        // Convert to milliseconds and round to 3 decimal places
+        out_means.push_back(std::round(mean) * 1e-3);
+        out_sd.push_back(std::round(std::sqrt(variance * 1e-6) * 1e+3) * 1e-3);
+        out_counts.push_back(count);
       }
 
       DataFrame df = DataFrame::create(
-          Named("Name") = unique_tags,
+          Named("Name") = out_tags,
           Named("Milliseconds") = out_means,
-          Named("SD") = out_sds,
-          Named("Count") = counts);
+          Named("SD") = out_sd,
+          Named("Count") = out_counts);
 
       Environment env = Environment::global_env();
       env[name] = df;
+    }
+
+    void reset()
+    {
+      data.clear();
     }
 
     // Destructor
