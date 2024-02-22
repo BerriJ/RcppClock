5e7053e57fc1080f42fe9a3d7c580a476daa0ca4
Jonathan Berrisch <Jonathan@Berrisch.biz>
2024-02-22 17:37:43 +0100

    Improve tests

--- tests/testthat/test_rcppclock.R
+++ tests/testthat/test_rcppclock.R
@@ -2,9 +2,9 @@ fibonacci(n = (1:4) * 10, reps = 10)
 
 expect_false(is.null(times))
 expect_true(all(!is.na(times)))
-expect_true(all(times$Milliseconds >= 0))
-expect_true(all(times$SD >= 0))
-expect_true(all(times$Count > 0))
+expect_gte(min(times$Milliseconds), 0)
+expect_gte(min(times$SD), 0)
+expect_gt(min(times$Count), 0)
 
 times_sc <- times
 
@@ -12,11 +12,11 @@ fibonacci_omp(n = (1:4) * 10, reps = 10)
 
 expect_false(is.null(times))
 expect_true(all(!is.na(times)))
-expect_true(all(times$Milliseconds >= 0))
-expect_true(all(times$SD >= 0))
-expect_true(all(times$Count > 0))
+expect_gte(min(times$Milliseconds), 0)
+expect_gte(min(times$SD), 0)
+expect_gt(min(times$Count), 0)
 
 times_mc <- times
 
 # We expect at least 90% speedup with OpenMP using 2 cores
-expect_true((times_sc$Milliseconds[1] >= times_mc$Milliseconds[1] * 2))
+expect_gt(times_sc$Milliseconds[1], times_mc$Milliseconds[1] * 1.9)
