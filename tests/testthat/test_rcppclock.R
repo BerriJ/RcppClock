29e3ceddddf97b39be591ccd888c1550bf72dea6
Jonathan Berrisch <jonathan@berrisch.biz>
2024-02-29 18:06:49 +0100

    Strip down tests
    For CRAN checks to not complain about exec time

--- tests/testthat/test_rcppclock.R
+++ tests/testthat/test_rcppclock.R
@@ -1,4 +1,4 @@
-fibonacci(n = rep(10 * (1:4), 10))
+fibonacci(n = rep(10 * (1:4), 3))
 
 expect_false(is.null(times))
 expect_true(all(!is.na(times)))
@@ -8,18 +8,10 @@ expect_gt(min(times$Count), 0)
 
 times_sc <- times
 
-fibonacci_omp(n = rep(10 * (1:4), 10))
+fibonacci_omp(n = rep(10 * (1:4), 3))
 
 expect_false(is.null(times))
 expect_true(all(!is.na(times)))
 expect_gte(min(times$Milliseconds), 0)
 expect_gte(min(times$SD), 0)
 expect_gt(min(times$Count), 0)
-
-times_mc <- times
-
-# We test if there is at least 50% speedup (to test if OMP works)
-test_that("Skip on MacOS", {
-    skip_on_os("mac")
-    expect_gt(times_sc$Milliseconds[5], times_mc$Milliseconds[5] * 1.5)
-})
