b0096a808bef9887ce55dc70911e4d00dd157733
Jonathan Berrisch <Jonathan@Berrisch.biz>
2024-08-08 15:04:19 +0200

    Fix tests
    expect_true did not eport errors in cases where data was NULL. Using expect_equal is fixing this.

--- tests/testthat/test_print.rcpptimer.R
+++ tests/testthat/test_print.rcpptimer.R
@@ -9,58 +9,62 @@ expect_s3_class(times, "data.frame")
 times$Microseconds <- c(0.5, 0.5, 0.5)
 
 # We expect that the print method scales the timings to nanoseconds
-p_out <- print(times)
+expect_output(p_out <- print(times))
 
 testthat::expect_contains(colnames(p_out), "Nanoseconds")
-expect_true(all(p_out$Nanoseconds == 500))
+expect_equal(p_out$Nanoseconds, c(500, 500, 500))
 
 # Spread timings between nanoseconds and seconds
 times$Microseconds <- c(2e+9, 1e-2, 1)
 
 # We do not expect scaling here
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Microseconds")
-expect_true(all(p_out$Nanoseconds == c(2e+9, 1e-2, 1)))
+expect_equal(p_out$Microseconds, c(2e+9, 1e-2, 1))
 
 # Set all timings to 5 Milliseconds
 times$Microseconds <- c(5e+3, 5e+3, 5e+3)
 
 # We expect that the print method scales the timings to milliseconds
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Milliseconds")
-expect_true(all(p_out$Milliseconds == 5))
+expect_equal(p_out$Milliseconds, c(5, 5, 5))
 
 # Set all timings to 5 Seconds
 times$Microseconds <- c(5e+6, 5e+6, 5e+6)
 
 # We expect that the print method scales the timings to seconds
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Seconds")
-expect_true(all(p_out$Seconds == 5))
+expect_equal(p_out$Seconds, c(5, 5, 5))
 
 # Set all timings to 5 Minutes
 times$Microseconds <- c(5 * 60 * 1e+6, 5 * 60 * 1e+6, 5 * 60 * 1e+6)
 
 # We expect that the print method scales the timings to minutes
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Minutes")
-expect_true(all(p_out$Minutes == 5))
+expect_equal(p_out$Minutes, c(5, 5, 5))
 
 # Set all timings to 5 Hours
-times$Microseconds <- c(5 * 60 * 60 * 1e+6, 5 * 60 * 60 * 1e+6, 5 * 60 * 60 * 1e+6)
+times$Microseconds <- c(
+  5 * 60 * 60 * 1e+6, 5 * 60 * 60 * 1e+6, 5 * 60 * 60 * 1e+6
+)
 
 # We expect that the print method scales the timings to hours
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Hours")
-expect_true(all(p_out$Hours == 5))
+expect_equal(p_out$Hours, c(5, 5, 5))
 
 # Set all timings to 5 Days
-times$Microseconds <- c(5 * 24 * 60 * 60 * 1e+6, 5 * 24 * 60 * 60 * 1e+6, 5 * 24 * 60 * 60 * 1e+6)
+times$Microseconds <- c(
+  5 * 24 * 60 * 60 * 1e+6, 5 * 24 * 60 * 60 * 1e+6, 5 * 24 * 60 * 60 * 1e+6
+)
 
 # We expect that the print method scales the timings to hours
-p_out <- print(times)
+expect_output(p_out <- print(times))
 testthat::expect_contains(colnames(p_out), "Hours")
-expect_true(all(p_out$Hours == 120))
+expect_equal(p_out$Hours, c(120, 120, 120))
 
 # Check if setting the scale argument to FALSE works
 
@@ -70,15 +74,15 @@ expect_true(all(p_out$Hours == 120))
 times$Microseconds <- c(0.5, 0.5, 0.5)
 
 # We expect that the print method does not scale if scale = FALSE
-p_out <- print(times, scale = FALSE)
+expect_output(p_out <- print(times, scale = FALSE))
 
 testthat::expect_contains(colnames(p_out), "Microseconds")
-expect_true(all(p_out$Nanoseconds == c(0.5, 0.5, 0.5)))
+expect_equal(p_out$Microseconds, c(0.5, 0.5, 0.5))
 
 # Set all timings to 5 Milliseconds
 times$Microseconds <- c(5e+3, 5e+3, 5e+3)
 
 # We expect that the print method does not scale if scale = FALSE
-p_out <- print(times, FALSE)
+expect_output(p_out <- print(times, FALSE))
 testthat::expect_contains(colnames(p_out), "Microseconds")
-expect_true(all(p_out$Milliseconds == c(5e+3, 5e+3, 5e+3)))
+expect_equal(p_out$Microseconds, c(5e+3, 5e+3, 5e+3))
