76db26c5625258daadb37eddffa4f9fc2fbddfb1
Jonathan Berrisch <Jonathan@Berrisch.biz>
2024-09-06 02:03:28 +0200

    Enclose tests in test_that()

--- tests/testthat/test_stats.R
+++ tests/testthat/test_stats.R
@@ -1,31 +1,33 @@
-foo <- test_stats(100, 10)
+test_that("Summary statistics are calculated correctly", {
+  foo <- test_stats(100, 10)
 
-means <- as.numeric(tapply(foo[[1]], foo[[2]], mean))
-sds <- as.numeric(tapply(foo[[1]], foo[[2]], sd))
-mins <- as.numeric(tapply(foo[[1]], foo[[2]], min))
-maxs <- as.numeric(tapply(foo[[1]], foo[[2]], max))
+  means <- as.numeric(tapply(foo[[1]], foo[[2]], mean))
+  sds <- as.numeric(tapply(foo[[1]], foo[[2]], sd))
+  mins <- as.numeric(tapply(foo[[1]], foo[[2]], min))
+  maxs <- as.numeric(tapply(foo[[1]], foo[[2]], max))
 
-# Unfortunately we cannot expect identical here, due to precision issues ...
-# Therefore, we added a small tolerance
+  # Unfortunately we cannot expect identical here, due to precision issues ...
+  # Therefore, we added a small tolerance
 
-expect_equal(
-  times$Microseconds,
-  as.numeric(round(means) * 1e-3),
-  tolerance = 2e-5
-)
+  expect_equal(
+    times$Microseconds,
+    as.numeric(round(means) * 1e-3),
+    tolerance = 2e-5
+  )
 
-expect_equal(
-  times$SD,
-  as.numeric(round(sds) * 1e-3),
-  tolerance = 2e-5
-)
+  expect_equal(
+    times$SD,
+    as.numeric(round(sds) * 1e-3),
+    tolerance = 2e-5
+  )
 
-expect_identical(
-  times$Max,
-  as.numeric(maxs) * 1e-3
-)
+  expect_identical(
+    times$Max,
+    as.numeric(maxs) * 1e-3
+  )
 
-expect_identical(
-  times$Min,
-  as.numeric(mins) * 1e-3
-)
+  expect_identical(
+    times$Min,
+    as.numeric(mins) * 1e-3
+  )
+})
--- tests/testthat/test_warnings.R
+++ tests/testthat/test_warnings.R
@@ -1,36 +1,38 @@
-# We expect that a warning is thrown
-expect_warning(test_misc(toc = FALSE, autoreturn = FALSE),
-  'Timer "t2" not stopped yet.',
-  ignore.case = FALSE
-)
+test_that("Warnings", {
+  # We expect that a warning is thrown
+  expect_warning(test_misc(toc = FALSE, autoreturn = FALSE),
+    'Timer "t2" not stopped yet.',
+    ignore.case = FALSE
+  )
 
-# We expect, despite that warning, times is still returned
-expect_contains(ls(as.environment(".GlobalEnv")), "times")
-rm(times, envir = as.environment(".GlobalEnv"))
+  # We expect, despite that warning, times is still returned
+  expect_contains(ls(as.environment(".GlobalEnv")), "times")
+  rm(times, envir = as.environment(".GlobalEnv"))
 
-# Test if a warning is thrown when verbose = true (default) and tic is missing
+  # Test if a warning is thrown when verbose = true (default) and tic is missing
 
-# We expect, despite that warning, times is still returned
-expect_warning(test_misc(tic = FALSE),
-  'Timer "t2" not started yet.',
-  ignore.case = FALSE
-)
+  # We expect, despite that warning, times is still returned
+  expect_warning(test_misc(tic = FALSE),
+    'Timer "t2" not started yet.',
+    ignore.case = FALSE
+  )
 
-# We expect, despite that warning, times is still returned
-expect_contains(ls(as.environment(".GlobalEnv")), "times")
-rm(times, envir = as.environment(".GlobalEnv"))
+  # We expect, despite that warning, times is still returned
+  expect_contains(ls(as.environment(".GlobalEnv")), "times")
+  rm(times, envir = as.environment(".GlobalEnv"))
 
-# Test if no warning is thrown when verbose = false and toc is missing
+  # Test if no warning is thrown when verbose = false and toc is missing
 
-expect_no_warning(test_misc(toc = FALSE, verbose = FALSE))
+  expect_no_warning(test_misc(toc = FALSE, verbose = FALSE))
 
-# We expect, despite that warning, times is still returned
-expect_contains(ls(as.environment(".GlobalEnv")), "times")
-rm(times, envir = as.environment(".GlobalEnv"))
+  # We expect, despite that warning, times is still returned
+  expect_contains(ls(as.environment(".GlobalEnv")), "times")
+  rm(times, envir = as.environment(".GlobalEnv"))
 
-# Test no a warning is thrown when verbose = false and tic is missing
-expect_no_warning(test_misc(tic = FALSE, verbose = FALSE))
+  # Test no a warning is thrown when verbose = false and tic is missing
+  expect_no_warning(test_misc(tic = FALSE, verbose = FALSE))
 
-# We expect, despite that warning, times is still returned
-expect_contains(ls(as.environment(".GlobalEnv")), "times")
-rm(times, envir = as.environment(".GlobalEnv"))
+  # We expect, despite that warning, times is still returned
+  expect_contains(ls(as.environment(".GlobalEnv")), "times")
+  rm(times, envir = as.environment(".GlobalEnv"))
+})
